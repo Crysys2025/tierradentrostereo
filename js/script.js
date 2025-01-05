d8453374da0ace4c25e69cec658c766316694c51
Crysys <cristianmedina2011@gmail.com>
2025-01-04 21:36:29 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 const RADIO_NAME = 'Tierradentro Stereo';
 
 // Change Stream URL Here, Supports, ICECAST, ZENO, SHOUTCAST, RADIOJAR and any other stream service.
-const URL_STREAMING = 'https://stream.zeno.fm/vt029ye33mru';
+const URL_STREAMING = 'https://stream.zeno.fm/vt029ye33mruv';
 
 // You can find the mount point in the Broadcast Settings.
 // To generate the Zeno Radio API link from the mount point,
